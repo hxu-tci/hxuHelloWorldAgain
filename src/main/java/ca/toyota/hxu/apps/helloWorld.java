26b1e050519be55dbd1b67728053a792487698eb
xuh <xuh@WD003124.tci.internal.toyota.ca>
2017-11-07 17:03:30 -0500

    changes in helloWorld!

--- src/main/java/ca/toyota/hxu/apps/helloWorld.java
+++ src/main/java/ca/toyota/hxu/apps/helloWorld.java
@@ -3,5 +3,6 @@ package ca.toyota.hxu.apps;
 public class helloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hello World again!");
 	}
 }
