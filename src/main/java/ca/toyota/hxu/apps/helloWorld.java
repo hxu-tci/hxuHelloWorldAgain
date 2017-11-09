4a713aff38740ca6b058badb84fc17442be4b8a8
Harry Xu <harry_xu@toyota.ca>
2017-11-09 14:38:46 +0000

    Update helloWorld.java

--- src/main/java/ca/toyota/hxu/apps/helloWorld.java
+++ src/main/java/ca/toyota/hxu/apps/helloWorld.java
@@ -2,7 +2,7 @@ package ca.toyota.hxu.apps;
 
 public class helloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World hxu");
 		System.out.println("Hello World again!");
 	}
 }
